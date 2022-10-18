868761bb0e2433af2b55827ad0e4319ecca9c4fa
LauricioX <developerwebmasterpro@gmail.com>
2022-10-18 16:43:58 -0300

    inicialização api

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const express = require("express");
+const app = express();
+
+
+
+app.get("/", (req,res) =>{
+res.send("lauricio o melhor programador do mundo 2023");
+
+}
+)
+
+app.listen(process.env.PORT || 3000, () =>{
+console.log("servidor rodando!")
+});

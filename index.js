15520e2b305119f421b8fe84b99bafa88224cf33
LauricioX <developerwebmasterpro@gmail.com>
2022-10-19 07:27:12 -0300

    carros

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ require("dotenv").config()
 const express = require("express");
 const app = express();
 
+const carros = [
+  {modelo:"hillux",marca:"toyota",ano:2022},
+  {modelo:"gol",marca:"ford",ano:2021},
+  {modelo:"fiesta",marca:"fiat",ano:2012},
+  {modelo:"uno",marca:"fiat",ano:2012}
+];
+
 
 
 app.get("/", (req,res) =>{
-res.send("lauricio o melhor programador do mundo 2023");
+res.json(carros)
 
 }
 );
